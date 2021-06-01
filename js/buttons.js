6ca89ec3c9da759414aad59f0642b2ec2a0cf485
Andrew <andrewtam0077@gmail.com>
2021-06-01 17:50:09 -0400

    added routing for the buttons to go to their destinations

--- js/buttons.js
+++ js/buttons.js
@@ -18,3 +18,14 @@ function hide(id) {
 function show(id) {
     document.getElementById(id).style.display = "block";
 }
+function goHome() {
+    if(confirm("Are you sure you want to leave?") == true) {
+        location.href = "index.html";
+    }
+}
+function goLobby() {
+    location.href = "lobby.html";
+}
+function goGame() {
+    location.href = "ingame.html";
+}
