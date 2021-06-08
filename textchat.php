28b3277db7956a33137d1b4ca24741aaff0a189e
hpalcaine <53156064+hpalcaine@users.noreply.github.com>
2021-06-08 18:31:12 -0400

    Add files via upload

--- textchat.php
+++ textchat.php
@@ -1,25 +1,49 @@
-<!-- Stylesheets Scripts and Resources -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-<link href="css/style.css" rel="stylesheet" type="text/css"/>
-<script src="js/buttons.js"></script>
-
 <div class="fitImg" style="width: 100%; height: 100%; 
-                            background-position: bottom 0 right 0; 
+                            background-position: top 0 left 0; 
                             background-size: auto 100%; background-image: url('images/textChat.png')">
     <h1 style="margin-left: 45%; padding-top: 3%" >Text Chat</h1>
-    <div style="margin: 0 5% 5% 37%; padding-top: 5%; height: 57%">
-        <table id="textchat" class="mt-auto">
-            <thead>
-            </thead>                    
-        </table>
+    <div style="margin: 0 5% 5% 25%; padding-top: 5%; height: 57%">
+        <?php 
+            //create a server side session
+            if(!isset($_COOKIE['textlist'])) {
+                setcookie('textlist', array(), time()+3600);
+            }
+            foreach($_COOKIE['textist'] as $user => $msg) {
+                $color = 'color: black;'
+                if($user == $_COOKIE['username'])
+                    $color = 'color: var(--darkred)';
+                ?>
+                <p style=" <?php echo $color ?>"> <?php echo $user . ": " . $msg ?> </p>
+                <?php
+            }
+        ?>
     </div>
-    <div style="margin: 0 5% 5% 37%;">
-        <form name="textform">
+    <div style="margin: 0 5% 5% 25%;">
+        <form name="textform" action="<?php $_SERVER['PHP_SELF']?>" method="get">
             <div class="form-group">
                 <textarea id="sendtext" class="form-control" rows="2" cols="40" placeholder = "type here..." style="border-radius: 7px;"></textarea>
             </div>
-            <input type="button" class="btn btn-dark btn-block" id="add" value="Send" onclick="sendText('You')"/> 
-            <span class="error" id="text_errmsg" style="color: red;"></span>
+            <input type="button" class="btn btn-dark btn-block" id="add" value="Send"/> 
         </form> 
+        <?php 
+            //These should be creating and modifying server sessions NOT COOKIES
+            if($_SERVER['REQUEST_METHOD'] == "GET") {
+                $text_errMsg = "";
+                if(isset($_GET['sendtext']) {
+                    if(strlen($_GET['sendtext']) > 280) {
+                        $text_errMsg = "Message is too long to send";
+                    }
+                } else {
+                    $text_errMsg = "Enter text to send a message";
+                }
+                if($text_errMsg == "") {
+                    $textlist = $_COOKIE['textlist']
+                    array_push($textlist, "you" => $_GET['sendtext'])
+                    setcookie('textlist', $textlist, time()+3600);
+                } else { ?>
+                    <span class="error" id="text_errmsg" style="color: red;"> <?php echo $text_errMsg ?></span>
+                <?php}
+            }
+        ?>
     </div>
 </div>
